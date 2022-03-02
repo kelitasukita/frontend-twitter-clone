ed6f95470e633d7bee7d4c8ef21f6627d0fb0eb8
Kelita Lamar <kelitasukita@gmail.com>
2022-03-02 01:41:04 +0100

    First commit

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Layout: React.FC = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <MenuBar />
+        <Main />
+        <SideBar />
+      </Wrapper>
+    </Container>
+  );
+}
+
+export default Layout;

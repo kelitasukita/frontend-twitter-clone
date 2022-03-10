5ab35dcb20742d53aa9871ed163727c05f3a57e8
Kelita Lamar <kelitasukita@gmail.com>
2022-03-10 23:20:32 +0100

    Add and apply style for bottom menu

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Container, Header, BackIcon, ProfileInfo } from './styles';
+import { Container, Header, BackIcon, ProfileInfo, BottomMenu, HomeIcon, SearchIcon, BellIcon, EmailIcon } from './styles';
 
 const Main: React.FC = () => {
   return (
@@ -17,13 +17,13 @@ const Main: React.FC = () => {
       </Header>
 
       {/* <ProfilePage /> */}
-{/* 
-      <BottonMenu>
-        <HomeIcon />
+
+      <BottomMenu>
+        <HomeIcon className="active"/>
         <SearchIcon />
         <BellIcon />
         <EmailIcon />
-      </BottonMenu> */}
+      </BottomMenu>
 
     </Container>
   );

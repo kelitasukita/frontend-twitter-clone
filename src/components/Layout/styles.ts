d6262642d75528f845acda6f3268b99a1aff6fdd
Kelita Lamar <kelitasukita@gmail.com>
2022-03-10 16:41:08 +0100

    Add coments to learn about elements

--- src/components/Layout/styles.ts
+++ src/components/Layout/styles.ts
@@ -7,9 +7,9 @@ export const Container = styled.div`
 export const Wrapper = styled.div`
   height: 100%;
   max-width: 1280px;
-  margin: 0 auto;
+  margin: 0 auto; /* Margem 0 para baixo e pra cima e auto para direita e esquerda */
 
-  display: flex;
-  justify-content: center;
+  display: flex; /* Trabalha os componentes lado a lado | 1, 2, 3 |*/
+  justify-content: center; /* Centraliza o conteúdo do layout */
 
   `;
